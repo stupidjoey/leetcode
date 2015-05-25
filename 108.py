f796f974ac6bcc309f0a1b03083925ffb7d132f7
stupidjoey <stupidzy1991@gmail.com>
2015-05-25 17:10:08 +0800

    updat

---
+++ 108.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {TreeNode}
+    def sortedArrayToBST(self, nums):
+        len_num = len(nums)
+        if len_num == 0 :
+            return None
+        m = len_num/2
+        root = TreeNode(nums[m])
+        root.left = self.sortedArrayToBST(nums[:m])
+        root.right = self.sortedArrayToBST(nums[m+1:])
+        
+        return root
+        
