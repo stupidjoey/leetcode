5cc18efbdeb19cf6080da0e36bbafe8b6a4064fc
stupidjoey <stupidzy1991@gmail.com>
2015-06-19 17:27:34 +0800

    new solution

--- 27.py
+++ 27.py
@@ -2,6 +2,21 @@ class Solution:
     # @param {integer[]} nums
     # @param {integer} val
     # @return {integer}
+    
+    # original solution
     def removeElement(self, nums, val):
         nums[:] = [x for x in nums if x != val]
         return len(nums)
+        
+    
+    # new solution
+    def removeElement(self, nums, val):
+        n = len(nums)
+        j = 0
+        for i in range(n):
+            cur_num = nums[i]
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
--- 75.py
+++ 75.py
@@ -11,8 +11,27 @@ class Solution:
         newnum = []
         for key in [0,1,2]:
             count = numdict[key]
-            newnum.extend( [key] * count)
-            
+            newnum.extend( [key] * count )            
         for i in range(len(nums)):
             nums[i] = newnum[i]
-        
+    
+    
+    #  new solution : two pointers
+    def sortColors(self, nums):
+        n  = len(nums)
+        i = 0
+        j = n
+        k = 0
+        while k < j and k >= 0:
+            if nums[k] < 1:
+                temp = nums[i]
+                nums[i] = nums[k]
+                nums[k] = temp
+                i += 1
+            elif nums[k] > 1:
+                j -= 1
+                temp = nums[j]
+                nums[j] = nums[k]
+                nums[k] = temp
+                k -= 1
+            k += 1
