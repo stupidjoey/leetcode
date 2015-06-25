93e5086485e015b7f5c1d42396bc10a4aee44f25
stupidjoey <stupidzy1991@gmail.com>
2015-06-25 13:12:17 +0800

    new solution

--- 142.py
+++ 142.py
@@ -20,5 +20,31 @@ class Solution:
                 p.val = markval
                 p = p.next
         return None
+    
+
+    # new solution  two pointers
+    def detectCycle(self, head):
+        if head == None:
+            return None
+
+        fast = head
+        slow = head
         
+        while fast!= None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+            
+        if fast == None or fast.next == None:
+            return None
+        
+        slow = head
+        while slow != fast:
+            fast = fast.next
+            slow = slow.next
+            
+        return slow
+
+
             
