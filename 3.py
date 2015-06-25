ae2c586a6cf3703513a78aff1bc7f60c8a5979ba
stupidjoey <stupidzy1991@gmail.com>
2015-06-25 23:39:22 +0800

    new solution

--- 3.py
+++ 3.py
@@ -13,4 +13,17 @@ class Solution:
                 stack = stack[idx+1:]
                 stack.append(c)
         return max_sublen
-                
+    
+    # new solution 
+    def lengthOfLongestSubstring(self, s):
+        i = 0
+        smap = {}
+        max_len = 0
+        n = len(s)
+        for k in range(n):
+            c = s[k]
+            if c in smap:
+                i = max(smap[c]+1, i)
+            smap[c] = k
+            max_len = max(max_len, k-i+1)
+        return max_len
