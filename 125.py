949a60ef6a617b6a9af89bfd3656ed704a9802ec
stupidjoey <stupidzy1991@gmail.com>
2015-06-25 17:32:01 +0800

    new solution

--- 125.py
+++ 125.py
@@ -19,8 +19,22 @@ class Solution:
                     return False
             return True
         
-        
-
+    # new solution     
+    def isPalindrome(self, s):
+        n = len(s)
+        i = 0
+        j = n-1
+        while i < j:
+            while i < j and  (not s[i].isalnum()):
+                i += 1
+            while i < j and ( not s[j].isalnum()):
+                j -= 1
+            if s[i].lower() != s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+        return True
 
         
 s1 = "A man, a plan, a canal: Panama"
